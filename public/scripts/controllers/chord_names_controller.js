dde01b1d70690dabf3f8d9d0c75c9b1e7cb20cd3
andyfehrenbach <andy.fehrenbach@gmail.com>
2016-03-09 10:37:52 -0600

    chord_names wireframe chord cards are done.

--- public/scripts/controllers/chord_names_controller.js
+++ public/scripts/controllers/chord_names_controller.js
@@ -1,3 +1,32 @@
 myApp.controller('Chord_namesController', ['$scope', '$http', function($scope, $http) {
     console.log('the Chord Names controller is working');
+
+    $scope.chords = [
+      {name: 'C'},
+      {name: 'D'},
+      {name: 'Em'},
+      {name: 'F'},
+      {name: 'G'},
+      {name: 'Am'},
+      {name: 'B7'}
+
+    ];
+
+    function CarouselDemoCtrl($scope){
+  $scope.myInterval = 3000;
+  $scope.slides = [
+    {
+      image: 'http://lorempixel.com/400/200/'
+    },
+    {
+      image: 'http://lorempixel.com/400/200/food'
+    },
+    {
+      image: 'http://lorempixel.com/400/200/sports'
+    },
+    {
+      image: 'http://lorempixel.com/400/200/people'
+    }
+  ];
+}
 }]);
