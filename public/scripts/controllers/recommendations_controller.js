180607f5119a4335c1d6a9cc0e2bcda3240140ee
andyfehrenbach <andy.fehrenbach@gmail.com>
2016-03-16 14:23:20 -0500

    added key recommendations in recommendations controller.

--- public/scripts/controllers/recommendations_controller.js
+++ public/scripts/controllers/recommendations_controller.js
@@ -19,6 +19,54 @@ $scope.allKeys = [
                      {Third: 'E', Capo: 7},
                      {Fourth: 'D', Capo: 9}]
   },
+  {Key: 'C',
+   Recommendations: [{First: 'C', Capo: 0},
+                     {Second: 'A', Capo : 3},
+                     {Third: 'G', Capo: 5},
+                     {Fourth: 'E', Capo: 8}]
+  },
+  {Key: 'C#',
+   Recommendations: [{First: 'C', Capo: 1},
+                     {Second: 'A', Capo : 4},
+                     {Third: 'G', Capo: 6},
+                     {Fourth: 'E', Capo: 9}]
+  },
+  {Key: 'D',
+   Recommendations: [{First: 'D', Capo: 0},
+                     {Second: 'C', Capo : 2},
+                     {Third: 'A', Capo: 5},
+                     {Fourth: 'G', Capo: 7}]
+  },
+  {Key: 'D#',
+   Recommendations: [{First: 'D', Capo: 1},
+                     {Second: 'C', Capo : 3},
+                     {Third: 'A', Capo: 6},
+                     {Fourth: 'G', Capo: 8}]
+  },
+  {Key: 'E',
+   Recommendations: [{First: 'E', Capo: 0},
+                     {Second: 'D', Capo : 2},
+                     {Third: 'C', Capo: 4},
+                     {Fourth: 'A', Capo: 7}]
+  },
+  {Key: 'F',
+   Recommendations: [{First: 'E', Capo: 1},
+                     {Second: 'D', Capo : 3},
+                     {Third: 'C', Capo: 5},
+                     {Fourth: 'A', Capo: 8}]
+  },
+  {Key: 'F#',
+   Recommendations: [{First: 'E', Capo: 2},
+                     {Second: 'D', Capo : 4},
+                     {Third: 'C', Capo: 6},
+                     {Fourth: 'A', Capo: 9}]
+  },
+  {Key: 'G',
+   Recommendations: [{First: 'G', Capo: 0},
+                     {Second: 'E', Capo : 3},
+                     {Third: 'D', Capo: 5},
+                     {Fourth: 'C', Capo: 7}]
+  },
 ];
 
   $scope.recommendations = ['First', 'Second', 'Third', 'Fourth'];
