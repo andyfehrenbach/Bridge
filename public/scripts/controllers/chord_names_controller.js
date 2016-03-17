29122f850a8b0d59401d01ca013b5c80dcbc98ff
andyfehrenbach <andy.fehrenbach@gmail.com>
2016-03-17 10:41:27 -0500

    key array defining logic has been moved into a data factory so it is accessible by both controllers.

--- public/scripts/controllers/chord_names_controller.js
+++ public/scripts/controllers/chord_names_controller.js
@@ -3,11 +3,7 @@
 myApp.controller('Chord_namesController', ['$scope', '$http', 'DataFactory', function($scope, $http, DataFactory) {
 console.log('the Chord Names controller is working');
 
-// //
-// $scope.dataFactory.retrieveMajorChords();
-// $scope.dataFactory.retrieveMinorChords();
-// $scope.dataFactory.retrieveSeventhChords();
-// console.log();
+
 
 // selector variables
 $scope.selectedKey = 'C';
@@ -22,6 +18,19 @@ $scope.minorChords = undefined;
 $scope.seventhChords = undefined;
 $scope.allOpenKeys = [];
 
+if ($scope.dataFactory.retrieveAllOpenKeys() === undefined) {
+        // initial load
+        $scope.dataFactory.factoryGetAllChords().then(function() {
+            $scope.allOpenKeys = $scope.dataFactory.retrieveAllOpenKeys();
+            console.log($scope.allOpenKeys);
+              initializeC();
+        });
+    } else {
+          $scope.allOpenKeys = $scope.dataFactory.retrieveAllOpenKeys();
+          console.log('else clause', $scope.allOpenKeys);
+          initializeC();
+    }
+
 /////transposing logic variables
 $scope.allNotes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']; //12
 $scope.capoPosition = '0';
@@ -32,49 +41,12 @@ $scope.newKey = [];
 
 
 
-//// Start a whole bunch of data factory retrieval
-  //// handles the get route for major chords
 
-  // if ($scope.dataFactory.majorChords() === undefined) {
-          // initial load
-          $scope.dataFactory.retrieveMajorChords().then(function() {
-              $scope.majorChords = $scope.dataFactory.majorChords();
-              console.log($scope.majorChords);
 
-          });
-      // } else {
-          $scope.majorChords = $scope.dataFactory.majorChords();
-          // console.log($scope.majorChords);
+              // console.log($scope.allOpenKeys);
+              // ///call key of c on load for the heck of it
 
-      // }
 
-  //// handles the get route for minor chords
-  // if ($scope.dataFactory.minorChords() === undefined) {
-          // initial load
-          $scope.dataFactory.retrieveMinorChords().then(function() {
-              $scope.minorChords = $scope.dataFactory.minorChords();
-              // console.log($scope.minorChords);
-          });
-      // } else {
-          $scope.minorChords = $scope.dataFactory.minorChords();
-          // console.log($scope.minorChords);
-      // }
-
-      //// handles the get route for seventh chords
-  // if ($scope.dataFactory.seventhChords() === undefined) {
-          // initial load
-          $scope.dataFactory.retrieveSeventhChords().then(function() {
-              $scope.seventhChords = $scope.dataFactory.seventhChords();
-              writeAllKeys();
-              console.log($scope.allOpenKeys);
-              // console.log($scope.keyOfC);
-              // console.log($scope.keyOfA);
-              ///call key of c on load for the heck of it
-                createCharts($scope.keyOfC);
-                createSmallArray($scope.keyOfC);
-                $scope.newKey = addSuffix(transpose(smallArray, '0', $scope.chords));
-                console.log($scope.newKey);
-                smallArray = [];
 
               //
               $scope.getChords = function (selectedKey, allOpenKeys) {
@@ -92,30 +64,8 @@ $scope.newKey = [];
                 smallArray = [];
               };
 
-          });
-      // } else {
-          $scope.seventhChords = $scope.dataFactory.seventhChords();
-      // }
+
 ///// END OF DATA FACTORY RETRIEVAL
-
-//add musical numeral to each chord in key array.
-// function addNumeral (key) {
-//   for (var i = 0; i < key.length; i++) {
-//     key[i].numeral = i + 1;
-//   }
-// }
-
-function stripKeyInfo(key) {
-  for (var i = 0; i < key.length; i++) {
-    if (key[i].chord_name.indexOf('m') !== -1) {
-      // console.log('applied at key:', key[i].chord_name);
-      key[i].chord_name = key[i].chord_name.substring(0, key[i].chord_name.length - 1);
-    } else if (key[i].chord_name.indexOf('7') !== -1) {
-        // console.log('applied at key:', key[i].chord_name);
-      key[i].chord_name = key[i].chord_name.substring(0,key[i].chord_name.length - 1);
-    }
-  }
-}
 
 function createCharts(key) {
   var createChart = chartMaker();
@@ -129,11 +79,12 @@ function createCharts(key) {
   createChart(document.getElementById('7'), key[6].chord_info);
 }
 
-/////Begin Transposing logic
+///Begin Transposing logic
 console.log('transposer');
-var newIndex = 0;
 function transpose(keyArray, capoPosition, initialChordsObject) {
   console.log('transpose function running');
+  var newIndex = 0;
+
     var transposedArray = [];
     for (var i = 0; i < keyArray.length; i++) {
       newIndex = (findIndex(keyArray[i]) + parseInt(capoPosition)) % $scope.allNotes.length;
@@ -171,133 +122,12 @@ function createSmallArray (keyArray) {
   }
 }
 
-
-
-
-// begin writing keys
-// Write A
-function writeA() {
-    $scope.keyOfA = [
-       $scope.majorChords[0],
-       $scope.minorChords[2],
-       $scope.minorChords[4],
-       $scope.majorChords[5],
-       $scope.majorChords[7],
-       $scope.minorChords[9],
-       $scope.seventhChords[11]
-  ];
-    stripKeyInfo($scope.keyOfA);
-    $scope.allOpenKeys.push($scope.keyOfA);
-    return $scope.keyOfA;
+function initializeC () {
+  createCharts($scope.allOpenKeys[2]);
+    createSmallArray($scope.allOpenKeys[2]);
+    $scope.newKey = addSuffix(transpose(smallArray, '0', $scope.chords));
+    console.log($scope.newKey);
+    smallArray = [];
 }
-
-// Write B
-function writeB() {
-    $scope.keyOfB = [
-       $scope.majorChords[2],
-       $scope.minorChords[4],
-       $scope.minorChords[6],
-       $scope.majorChords[7],
-       $scope.majorChords[9],
-       $scope.minorChords[11],
-       $scope.seventhChords[1]
-  ];
-    stripKeyInfo($scope.keyOfB);
-    $scope.allOpenKeys.push($scope.keyOfB);
-    return $scope.keyOfB;
-}
-
-// Write C
-function writeC() {
-    $scope.keyOfC = [
-       $scope.majorChords[3],
-       $scope.minorChords[5],
-       $scope.minorChords[7],
-       $scope.majorChords[8],
-       $scope.majorChords[10],
-       $scope.minorChords[0],
-       $scope.seventhChords[2]
-  ];
-    stripKeyInfo($scope.keyOfC);
-    $scope.allOpenKeys.push($scope.keyOfC);
-    return $scope.keyOfC;
-}
-
-// Write D
-function writeD() {
-    $scope.keyOfD = [
-       $scope.majorChords[5],
-       $scope.minorChords[7],
-       $scope.minorChords[9],
-       $scope.majorChords[10],
-       $scope.majorChords[0],
-       $scope.minorChords[2],
-       $scope.seventhChords[4]
-  ];
-    stripKeyInfo($scope.keyOfD);
-    $scope.allOpenKeys.push($scope.keyOfD);
-    return $scope.keyOfD;
-}
-
-// Write E
-function writeE() {
-    $scope.keyOfE = [
-       $scope.majorChords[7],
-       $scope.minorChords[9],
-       $scope.minorChords[11],
-       $scope.majorChords[0],
-       $scope.majorChords[2],
-       $scope.minorChords[4],
-       $scope.seventhChords[6]
-  ];
-    stripKeyInfo($scope.keyOfE);
-    $scope.allOpenKeys.push($scope.keyOfE);
-    return $scope.keyOfE;
-}
-
-// Write F
-function writeF() {
-    $scope.keyOfF = [
-       $scope.majorChords[8],
-       $scope.minorChords[10],
-       $scope.minorChords[0],
-       $scope.majorChords[1],
-       $scope.majorChords[3],
-       $scope.minorChords[5],
-       $scope.seventhChords[7]
-  ];
-    stripKeyInfo($scope.keyOfF);
-    $scope.allOpenKeys.push($scope.keyOfF);
-    return $scope.keyOfF;
-}
-
-// Write G
-function writeG() {
-    $scope.keyOfG = [
-       $scope.majorChords[10],
-       $scope.minorChords[0],
-       $scope.minorChords[2],
-       $scope.majorChords[3],
-       $scope.majorChords[5],
-       $scope.minorChords[7],
-       $scope.seventhChords[9]
-  ];
-    stripKeyInfo($scope.keyOfG);
-    $scope.allOpenKeys.push($scope.keyOfG);
-    return $scope.keyOfG;
-}
-
-function writeAllKeys() {
-  writeA();
-  writeB();
-  writeC();
-  writeD();
-  writeE();
-  writeF();
-  writeG();
-}
-
-
-
 
 }]);
--- public/scripts/data_factory.js
+++ public/scripts/data_factory.js
@@ -3,13 +3,15 @@ myApp.factory('DataFactory', ['$http', function($http) {
     var majorChords;
     var minorChords;
     var seventhChords;
+    var allOpenKeys = undefined;
+
 
 
     var getMajorChords = function() {
         // console.log('getting chords from server');
         var promise = $http.get('/all_chords').then(function(response) {
             majorChords = response.data;
-            // console.log('response:major', majorChords);
+             console.log('response:major', majorChords);
         });
         return promise;
     };
@@ -18,7 +20,7 @@ myApp.factory('DataFactory', ['$http', function($http) {
         // console.log('getting minor chords from server');
         var promise = $http.get('/minor_chords').then(function(response) {
             minorChords = response.data;
-            // console.log('response: minor', minorChords);
+            console.log('response: minor', minorChords);
         });
         return promise;
     };
@@ -27,41 +29,177 @@ myApp.factory('DataFactory', ['$http', function($http) {
         // console.log('getting seventh chords from server');
         var promise = $http.get('/seventh_chords').then(function(response) {
             seventhChords = response.data;
-            // console.log('response: minor', seventhChords);
+            console.log('response: minor', seventhChords);
+
+
         });
         return promise;
     };
 
 
+function getAllChords() {
+
+  console.log('getAllChords is running');
+  return getMajorChords().then(function () {
+      return getMinorChords().then(function () {
+        return getSeventhChords().then(function () {
+          console.log('getAllChords is finished' );
+          writeAllKeys();
+          console.log('the array ', allOpenKeys);
+        });
+      });
+    });
+
+}
 
 
+    // begin writing keys
+    // Write A
+    function writeA() {
+        var keyOfA = [
+           majorChords[0],
+           minorChords[2],
+           minorChords[4],
+           majorChords[5],
+           majorChords[7],
+           minorChords[9],
+           seventhChords[11]
+      ];
+        stripKeyInfo(keyOfA);
+        allOpenKeys.push(keyOfA);
+        return keyOfA;
+    }
+
+    // Write B
+    function writeB() {
+        var keyOfB = [
+           majorChords[2],
+           minorChords[4],
+           minorChords[6],
+           majorChords[7],
+           majorChords[9],
+           minorChords[11],
+           seventhChords[1]
+      ];
+        stripKeyInfo(keyOfB);
+        allOpenKeys.push(keyOfB);
+        return keyOfB;
+    }
+
+    // Write C
+    function writeC() {
+        var keyOfC = [
+           majorChords[3],
+           minorChords[5],
+           minorChords[7],
+           majorChords[8],
+           majorChords[10],
+           minorChords[0],
+           seventhChords[2]
+      ];
+        stripKeyInfo(keyOfC);
+        allOpenKeys.push(keyOfC);
+        return keyOfC;
+    }
+
+    // Write D
+    function writeD() {
+        var keyOfD = [
+           majorChords[5],
+           minorChords[7],
+           minorChords[9],
+           majorChords[10],
+           majorChords[0],
+           minorChords[2],
+           seventhChords[4]
+      ];
+        stripKeyInfo(keyOfD);
+        allOpenKeys.push(keyOfD);
+        return keyOfD;
+    }
+
+    // Write E
+    function writeE() {
+        var keyOfE = [
+           majorChords[7],
+           minorChords[9],
+           minorChords[11],
+           majorChords[0],
+           majorChords[2],
+           minorChords[4],
+           seventhChords[6]
+      ];
+        stripKeyInfo(keyOfE);
+        allOpenKeys.push(keyOfE);
+        return keyOfE;
+    }
+
+    // Write F
+    function writeF() {
+        var keyOfF = [
+           majorChords[8],
+           minorChords[10],
+           minorChords[0],
+           majorChords[1],
+           majorChords[3],
+           minorChords[5],
+           seventhChords[7]
+      ];
+        stripKeyInfo(keyOfF);
+        allOpenKeys.push(keyOfF);
+        return keyOfF;
+    }
+
+    // Write G
+    function writeG() {
+        var keyOfG = [
+           majorChords[10],
+           minorChords[0],
+           minorChords[2],
+           majorChords[3],
+           majorChords[5],
+           minorChords[7],
+           seventhChords[9]
+      ];
+        stripKeyInfo(keyOfG);
+        allOpenKeys.push(keyOfG);
+        return keyOfG;
+    }
+
+    function stripKeyInfo(key) {
+      for (var i = 0; i < key.length; i++) {
+        if (key[i].chord_name.indexOf('m') !== -1) {
+          // console.log('applied at key:', key[i].chord_name);
+          key[i].chord_name = key[i].chord_name.substring(0, key[i].chord_name.length - 1);
+        } else if (key[i].chord_name.indexOf('7') !== -1) {
+            // console.log('applied at key:', key[i].chord_name);
+          key[i].chord_name = key[i].chord_name.substring(0,key[i].chord_name.length - 1);
+        }
+      }
+    }
+
+    function writeAllKeys() {
+      allOpenKeys = [];
+      writeA();
+      writeB();
+      writeC();
+      writeD();
+      writeE();
+      writeF();
+      writeG();
+    }
 
 
     //PUBLIC
 
     var publicApi = {
 
-        retrieveMajorChords: function() {
-            return getMajorChords();
+        retrieveAllOpenKeys: function () {
+          return allOpenKeys;
         },
-        majorChords: function () {
-            return majorChords;
-        },
-
-        retrieveMinorChords: function() {
-            return getMinorChords();
-        },
-        minorChords: function () {
-            return minorChords;
-        },
-
-        retrieveSeventhChords: function() {
-            return getSeventhChords();
-        },
-        seventhChords: function () {
-            return seventhChords;
+        factoryGetAllChords: function () {
+          return getAllChords();
         }
-
     };
 
     return publicApi;
